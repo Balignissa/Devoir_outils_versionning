023d0503a2fdddd463f014b94fb19843006f8e0f
Balignissa <balignissa@gmail.com>
2023-09-19 21:12:32 +0000

    verifier le nombre de classe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Devoir_outils_versionning
+
+Le programme permet de réaliser des tokens avec un fichier csv notament la colonne absbstrat en utilisant un tokennizer. Un fichier csv est ajouté dans le dossier pour cet effet il contient plusieurs collonnes.
--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 # BENANE Balignissa Master2  DATA SCIENCE
 
+import pandas as pd
+# Charger le fichier CSV
+data = pd.read_csv('multilabel-classification-datase.csv',encoding='UTF-8')
+print(data.info())
+print(data.head())
+
+# Vérifier les valeurs nulles dans chaque colonne
+null_counts = data.isnull().sum()
+print(null_counts)
+
+# Supprimer les lignes avec des valeurs nulles
+data_cleaned = data.dropna()
+
+
+import numpy as np
+seq_len=120
+num_samples = len(data)
+print(num_samples)
+
+
+# initialisation des  inputs ids et attention mask en array
+xids = np.zeros((num_samples,seq_len))
+xmasks = np.zeros((num_samples,seq_len))
+
+print(xids)
+
+# choisir le type de token et l'initialiser
+#!pip install git+https://github.com/huggingface/transformers
+from transformers import BertTokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+# Tokenizer les phrases
+for i, phrase in enumerate(data['ABSTRACT']):
+    tokens = tokenizer.encode_plus(phrase, max_length=seq_len, truncation=True,
+    padding = 'max_length', add_special_token=True, retur_tensor='tf')
+
+xids[i, :] = tokens['input_ids']
+xmasks[i, :] = tokens['attention_mask']
+
+print(xids)
+print(xmasks)
+labels = np.zeros((num_samples,6))
+labels
+
+import tensorflow as tf
+dataset = tf.data.Dataset.from_tensor_slices((xids, xmasks, labels))
+dataset.take(1) #prendre un seul exemple parmi plusieurs
+
+#Vérifier le nombre de classes
+labels[0, :].shape
